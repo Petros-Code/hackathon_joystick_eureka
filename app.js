fe7538d5c59f3a51bb5633f9cb94aeca185f0ea2
Petros-Code <petros-code@proton.me>
2025-08-05 10:22:50 +0200

    App.js de base avec 2 routes type health check

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import pool from "./config/db.js";
+
+const app = express();
+const port = 3000;
+
+//#region CHECK ETAT DU SERVEUR
+app.get("/bienvenue", (req, res) => {
+    res.json({ message: "Bienvenue sur le serveur EUREKA"})
+});
+app.get("/etat", async (req, res) => {
+    try {
+        await pool.query('SELECT 1');
+        res.status(200).json({ status : "OK", message : "Connecté à la DB" });
+    } catch (error) {
+        console.error("Erreur de connexion avc la DB :", error.message);
+        res.status(500).json ({ status : "ERREUR", message: "Erreur de connexio navec la DB" });
+    }
+});
+//#endregion
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server launched on http://localhost:${port}`);
+});
